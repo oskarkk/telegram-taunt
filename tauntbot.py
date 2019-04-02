b86408da166b72577c1841208016dd8f55689138
oskarkk <16339637+oskarkk@users.noreply.github.com>
2019-04-02 17:05:40 +0200

    Add prototype of program logic

--- tauntbot.py
+++ tauntbot.py
@@ -3,9 +3,10 @@ from config import *
 
 def getUpdates(token, timeout=0, lastUpdate=0, types=[], limit=100):
     dic = { 'timeout': timeout,
-            'allowed_updates': types
+            'allowed_updates': types,
+            'limit': limit
           }
-		  
+
     if lastUpdate:
         dic['offset'] = lastUpdate['update_id'] + 1
 
@@ -17,9 +18,22 @@ def getUpdates(token, timeout=0, lastUpdate=0, types=[], limit=100):
     if resp['result']:
         return resp['result']
     else:
-        return 0 # file here
+        return 0  # file here
+
+def compare(text):
+    return 0
+
+def sendAnswers(query,taunts):
+    return 0
+
+def saveStats(taunt):
+    pass
 
 while 1:
     data = getUpdates(botToken, 10)
     while data:
+        for update in data:
+            answers = compare(update['inline_query']['query'])
+            chosen = sendAnswers(update['inline_query']['id'], answers)
+            saveStats(chosen)
         data = getUpdates(botToken, 10, data[-1])
