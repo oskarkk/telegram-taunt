f5885a98d7a1f44a88cb5cc7b6349938046c00e6
oskarkk <16339637+oskarkk@users.noreply.github.com>
2019-04-05 03:19:34 +0200

    Fix ending newline bug

--- stats.py
+++ stats.py
@@ -4,11 +4,15 @@ def create(taunts):
         for x in range(len(taunts[1:])):
             f.write(str(x+1) + '\t0\n')
 
+# arg can be a list or a set
 def get(tauntList=0):
     with open('stats.txt', 'r') as f:
         content = f.read()
-    # [:-1] for deleting a newline at the end
-    lines = content[:-1].split('\n')
+    # deleting a newline at the end
+    if content[-1] == '\n': 
+        content = content[:-1]
+    # split lines to list of 2-element lists
+    lines = content.split('\n')
     lines = [ x.split('\t') for x in lines ]
     if not tauntList: return lines
     return [ lines[int(i)-1] for i in tauntList ]
